0f2318cc623a58fde4c78c5665fd8e783850edc6
tyokoyama <yokoyama4t9t23t@gmail.com>
2021-01-28 17:53:27 +0900

    sample

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+PRIORITY = (('danger', 'high'), ('info', 'normal'), ('success', 'low'))
 
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=PRIORITY
+    )
+    duedate = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.pk})
